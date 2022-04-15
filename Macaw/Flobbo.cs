ec2c3cfe73b8348a0f4affd753132eb92be6dc93
Sam Hou <SamHou20070203@outlook.com>
2022-04-15 21:47:08 +0800

    update

---
+++ Macaw/Flobbo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macaw
+{
+    internal class Flobbo
+    {
+        private string zap;
+        public Flobbo(string zap)
+        {
+            this.zap = zap;
+        }
+        public static StreamWriter Snobbo()
+        {
+            return new StreamWriter("macaw.txt");
+        }
+        public bool Blobbo(bool Already,StreamWriter sw)
+        {
+            if (Already)
+            {
+                sw.WriteLine(zap);
+                sw.Close();
+                return false;
+            }
+            else
+            {
+                sw.WriteLine(zap);
+                zap = "red orange";
+                return true;
+            }
+        }
+        public bool Blobbo(StreamWriter sw)
+        {
+            sw.WriteLine(zap);
+            zap = "green purple";
+            return false;
+        }
+    }
+}
---
+++ Macaw/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Macaw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Flobbo f = new("blue yellow");
+            StreamWriter sw = Flobbo.Snobbo();
+            f.Blobbo(f.Blobbo(f.Blobbo(sw), sw), sw);
+        }
+    }
+}
